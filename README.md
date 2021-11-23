c502ea81f4438b12ed874f7644ba5916c45fb43f
Lakshya Dhariwal <lakshyadhariwal9@gmail.com>
2021-11-23 19:41:28 +0530

    feat: search basic from multiple sites

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # srch - search from the terminal
 
 
+
+`
+? Pick site: (Use arrow keys)
+❯ google 
+  stack overflow 
+  youtube 
+  leetcode 
+  reddit 
+? Search Query`
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+let url = "https://www.google.com/search?q=";
+const open = require("open");
 const inquirer = require("inquirer");
 inquirer
   .prompt([
@@ -6,15 +8,39 @@ inquirer
       message: "Pick site:",
       name: "site",
       choices: ["google", "stack overflow", "youtube", "leetcode", "reddit"],
-    },{
-        type: "text",
-        message: "Search Query",
-        name: "query",
-    }
+    },
+    {
+      type: "text",
+      message: "Search Query",
+      name: "query",
+    },
   ])
   .then((answers) => {
-    console.log(answers);
+    const { site, query } = answers;
+    const queryString = encodeURIComponent(query);
+    search(site, queryString);
+    open(url);
   })
   .catch((error) => {
-    console.log(error)
+    console.log(error);
   });
+
+const search = (site, query) => {
+  switch (site) {
+    case "google":
+      url = `https://www.google.com/search?q=${query}`;
+      break;
+    case "stack overflow":
+      url = `https://stackoverflow.com/search?q=${query}`;
+      break;
+    case "youtube":
+      url = `https://www.youtube.com/search?q=${query}`;
+      break;
+    case "leetcode":
+      url = `https://leetcode.com/problemset/all/?search=${query}`;
+      break;
+    case "reddit":
+      url = `https://www.reddit.com/search?q=${query}`;
+      break;
+  }
+};
